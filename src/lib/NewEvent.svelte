f4a2d62169c0ecf28125e7951fb0bed8ce3c0acf
Pranay <pranay.patel@gupshup.io>
2023-03-28 00:51:38 +0530

    update event creation form ui

--- src/lib/NewEvent.svelte
+++ src/lib/NewEvent.svelte
@@ -13,71 +13,82 @@
   };
 </script>
 
-<div class=" flex flex-col items-center justify-center gap-9">
-  <h1>Satsang Event Creation Form</h1>
-  <form class="flex flex-col gap-2">
-    <div class="flex flex-row gap-4">
-      <div><Label class="block mb-2" for="name" />Name:</div>
-      <div><Input id="name" type="text" bind:value={$NewEventStore.name} /></div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="place" />Place:</div>
-      <div><Input id="place" type="text" bind:value={$NewEventStore.place} /></div>
-    </div>
-    <!-- svelte-ignore a11y-label-has-associated-control -->
-    <div class="flex flex-row gap-4">
-      <div><Label for="date" />Date:</div>
-      <div>
-        <DateInput closeOnSelection {locale} format="dd-MM-yyyy HH:mm" placeholder="Select Date 🗓️" bind:value={$NewEventStore.date} />
-      </div>
-    </div>
-    <div class="flex flex-row gap-4 ">
-      <div>
-        <Label for="MenuItems" />Menu Items:
-      </div>
-      <div>
-        <MultiSelect
-          id="MenuItems"
-          options={MenuItems}
-          placeholder="Add menu items..."
-          allowUserOptions={false}
-          selected={[]}
-          bind:value={$NewEventStore.menuItems}
-          on:add={(event) => {
-            // if you want to persist new user entered custom options to a database, perform
-            // a fetch request with type POST here using the event.detail.option payload
-            console.log(event.detail.option);
-          }}
-          required
-        >
-          <MenuItemSlot let:idx {idx} let:option {option} slot="selected" />
-          <MenuItemSlot let:idx {idx} let:option {option} slot="option" />
-        </MultiSelect>
-      </div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="host" />Host:</div>
-      <div><Input id="host" type="text" bind:value={$NewEventStore.host} /></div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="phone" />Phone Number:</div>
-      <div><Input id="phone" type="text" bind:value={$NewEventStore.phone} /></div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="count" />Count:</div>
-      <div><Input id="count" type="text" bind:value={$NewEventStore.count} /></div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="totalExpense" />Total Expense:</div>
-      <div><Input id="totalExpense" type="text" bind:value={$NewEventStore.totalExpense} /></div>
-    </div>
-    <div class="flex flex-row gap-4">
-      <div><Label for="receivedAmount" />Received Amount:</div>
-      <div><Input id="receivedAmount" type="text" bind:value={$NewEventStore.receivedAmount} /></div>
-    </div>
+<div class=" flex flex-col items-center justify-center gap-9 font-mono ">
+  <h1 class="my-4 text-2xl hover:font-bold">Satsang Event Creation Form</h1>
+  <div class="grid grid-cols-10 w-full">
+    <div class="" />
+    <div class="col-span-8">
+      <form>
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <div><Label class="block mb-2" for="name" />Name:</div>
+            <div><Input class="flex-1" id="name" type="text" bind:value={$NewEventStore.name} /></div>
+          </div>
 
-    <Button gradient color="purpleToBlue" on:click={handleSubmit}>Submit</Button>
-  </form>
+          <div>
+            <div><Label class="block mb-2" for="date" />Date:</div>
+            <div>
+              <DateInput closeOnSelection {locale} format="dd-MM-yyyy" placeholder="Select Date 🗓️" bind:value={$NewEventStore.date} />
+            </div>
+          </div>
+        </div>
+        <div class="mb-6">
+          <div><Label class="block mb-2" for="place" />Place:</div>
+          <div><Input id="place" type="text" bind:value={$NewEventStore.place} /></div>
+        </div>
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+
+        <div class="mb-6 ">
+          <div>
+            <Label for="MenuItems" />Menu Items:
+          </div>
+          <div class="">
+            <MultiSelect
+              id="MenuItems"
+              options={MenuItems}
+              placeholder="Add menu items..."
+              allowUserOptions={false}
+              selected={[]}
+              bind:value={$NewEventStore.menuItems}
+              on:add={(event) => {
+                // if you want to persist new user entered custom options to a database, perform
+                // a fetch request with type POST here using the event.detail.option payload
+                console.log(event.detail.option);
+              }}
+              required
+            >
+              <MenuItemSlot let:idx {idx} let:option {option} slot="selected" />
+              <MenuItemSlot let:idx {idx} let:option {option} slot="option" />
+            </MultiSelect>
+          </div>
+        </div>
+        <div class="mb-6">
+          <div><Label class="block mb-2" for="host" />Host:</div>
+          <div><Input id="host" type="text" bind:value={$NewEventStore.host} /></div>
+        </div>
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <div><Label class="block mb-2" for="phone" />Phone Number:</div>
+            <div><Input id="phone" type="text" bind:value={$NewEventStore.phone} /></div>
+          </div>
+          <div>
+            <div><Label class="block mb-2" for="count" />Count:</div>
+            <div><Input id="count" type="text" bind:value={$NewEventStore.count} /></div>
+          </div>
+          <div>
+            <div><Label class="block mb-2" for="totalExpense" />Total Expense:</div>
+            <div><Input id="totalExpense" type="text" bind:value={$NewEventStore.totalExpense} /></div>
+          </div>
+          <div>
+            <div><Label class="block mb-2" for="receivedAmount" />Received Amount:</div>
+            <div><Input id="receivedAmount" type="text" bind:value={$NewEventStore.receivedAmount} /></div>
+          </div>
+        </div>
+
+        <Button gradient color="purpleToBlue" on:click={handleSubmit}>Submit</Button>
+      </form>
+    </div>
+  </div>
   <p>
     {JSON.stringify($NewEventStore)}
   </p>
